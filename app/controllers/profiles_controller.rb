5c3e0968a081667603479b9e1f300196b51e7c8c
Yoann Kermet <y.kermet@gmail.com>
2026-06-02 13:00:50 +0200

    active storage and configuration with cloudinary

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,6 +21,6 @@ class ProfilesController < ApplicationController
   end
 
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :profession)
+    params.require(:profile).permit(:first_name, :last_name, :profession, :logo)
   end
 end
