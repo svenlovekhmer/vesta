7155aac9a7aa7e16a6936ab06be034ea5af50334
Yoann Kermet <y.kermet@gmail.com>
2026-06-02 13:31:00 +0200

    fix translations

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Vesta
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
-    config.i18n.default_locale = :fr
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
