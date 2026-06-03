e3d281e2afcf74646dbcd181e2804159380b79d4
Alexandre Theol <alexandre.theol@gmail.com>
2026-06-03 17:47:25 +0200

    fix heroku action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,11 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
+  config.action_cable.allowed_request_origins = [
+    /https?:\/\/.*\.herokuapp\.com/,
+    /https?:\/\/.*\.vesta\.com/
+  ]
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
