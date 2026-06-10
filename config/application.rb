18a0a0a1600efb71bda6ae09c44f587621f88904
Yoann Kermet <y.kermet@gmail.com>
2026-06-10 11:59:07 +0200

    errors page ok for test

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Vesta
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.exceptions_app = routes
     # config.i18n.default_locale = :fr
 
     # Configuration for the application, engines, and railties goes here.
